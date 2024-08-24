c6cc236eaaa0865e17015ac1a69fa89124901d2d
Jy0ti-360 <jyotipjenabbsr@gmail.com>
2024-08-24 19:52:57 +0530

    Add tagline for my bank.

--- BankingApplication/src/Bank.java
+++ BankingApplication/src/Bank.java
@@ -1,6 +1,7 @@
 public class Bank {
     public static void main(String[] args){
         System.out.println("$$$ WELCOME TO JYOTI'S BANK $$$");
+        System.out.println("Pay Securely");
         new BankUI();
         System.out.println("THANK YOU FOR VISITING...");
     }
